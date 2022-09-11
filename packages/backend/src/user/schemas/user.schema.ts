952c09cea033daec07b4e59a0a007fbad280e488
Daria Terekhova <daria.terekhova5@gmail.com>
2022-09-11 22:22:54 +0300

    Some documentation added

--- packages/backend/src/user/schemas/user.schema.ts
+++ packages/backend/src/user/schemas/user.schema.ts
@@ -1,16 +1,20 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { ApiProperty } from "@nestjs/swagger";
 import { Document } from "mongoose";
 
 export type UserDocument = User & Document
 
 @Schema()
 export class User {
+    @ApiProperty()
     @Prop({required: true})
     username: string;
 
+    @ApiProperty()
     @Prop({required: true})
     password: string;
 
+    @ApiProperty({required: false})
     @Prop()
     nameToShow: string;
 }
--- packages/backend/src/user/user.controller.ts
+++ packages/backend/src/user/user.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Get, Param, Put } from '@nestjs/common';
-import { User, UserSchema } from './schemas/user.schema';
+import { Body, Controller, Get, HttpCode, Param, Put } from '@nestjs/common';
+import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import { User } from './schemas/user.schema';
 import { UserService } from './user.service';
 
+@ApiTags('User')
 @Controller('user')
 export class UserController {
   constructor (private readonly userService: UserService) {
@@ -11,8 +13,13 @@ export class UserController {
   userInfo(@Param('id') id: string): Promise<User> {
     return this.userService.getUserByUsername(id)
   }
-
+  
   @Put()
+  @HttpCode(201)
+  @ApiCreatedResponse({
+    description: 'The record has been successfully created.',
+    type: User,
+  })
   addUser(@Body() user: User): Promise<User> {
     return this.userService.createUser(user)
   }
